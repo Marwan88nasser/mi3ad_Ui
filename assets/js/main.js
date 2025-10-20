d8d489606cf2bc06e9d860ed281b6bfb8ae17f53
Marwan88nasser <marwan.nasser565@gmail.com>
2025-10-20 09:25:20 +0300

    last update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,17 @@
-  var swiper = new Swiper(".mySwiper", {
-      speed: 600,
-      parallax: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
+new Swiper(".landing-swiper", {
+  speed: 1600,
+  parallax: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
