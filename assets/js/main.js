1937ce3ad06ed8510cbc622bc6845050b55fe72f
Marwan88nasser <marwan.nasser565@gmail.com>
2025-10-20 10:32:42 +0300

    last update

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,44 @@ new Swiper(".landing-swiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+new Swiper(".ads-swiper", {
+  // Autoplay مستمر
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+
+  // حركة دائرية مستمرة
+  loop: true,
+
+  // سرعة الانتقال
+  speed: 800,
+
+  // التأثير
+  effect: "slide",
+
+  // عدد الشرائح المعروضة
+  slidesPerView: 1,
+  spaceBetween: 20,
+
+  // Pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Responsive
+  // breakpoints: {
+  //   768: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 20,
+  //   },
+  //   1024: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 30,
+  //   },
+  // },
+});
